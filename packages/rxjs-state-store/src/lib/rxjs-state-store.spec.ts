7fec876321575b9df772feff23a95c6a3548319b
Marcell Kiss <kissmarcell88@gmail.com>
2023-02-21 17:21:44 +0100

    feat: initial version of rxjs-state-store

--- packages/rxjs-state-store/src/lib/rxjs-state-store.spec.ts
+++ packages/rxjs-state-store/src/lib/rxjs-state-store.spec.ts
@@ -1,7 +1,66 @@
-import { rxjsStateStore } from './rxjs-state-store';
+import { StateStore } from './rxjs-state-store';
 
 describe('rxjsStateStore', () => {
-  it('should work', () => {
-    expect(rxjsStateStore()).toEqual('rxjs-state-store');
+  it('StateStore with a number', () => {
+    // With number
+    const numberStore = new StateStore(1);
+    const spy = jest.fn();
+    numberStore.value$.subscribe(spy);
+    expect(spy).toHaveBeenCalledWith(1);
+
+    numberStore.update(2);
+    expect(spy).toHaveBeenCalledWith(2);
+  });
+
+  it('StateStore with a string', () => {
+    // With string
+    const stringStore = new StateStore<string | null>(null);
+    const spy = jest.fn();
+    stringStore.value$.subscribe(spy);
+    expect(spy).toHaveBeenLastCalledWith(null);
+
+    stringStore.update('Hello');
+    expect(spy).toHaveBeenLastCalledWith('Hello');
+  });
+
+  it('StateStore with an object', () => {
+    // With object
+    const initialObject = { name: 'Joe', age: 33 };
+    const objectStore = new StateStore(initialObject);
+    const spy = jest.fn();
+    objectStore.value$.subscribe(spy);
+    expect(spy).toHaveBeenLastCalledWith(initialObject);
+
+    objectStore.patch({ age: 34 });
+    expect(spy.mock.lastCall[0].age).toEqual(34);
+  });
+
+  it('StateStore with an array', () => {
+    const initialArray = [
+      { id: 1, name: 'Joe' },
+      { id: 2, name: 'Maria' },
+    ];
+    // Array store
+    const arrayStore = new StateStore(initialArray);
+    const spy = jest.fn();
+    arrayStore.value$.subscribe(spy);
+    expect(spy).toHaveBeenLastCalledWith(initialArray);
+
+    arrayStore.addItem({ id: 3, name: 'Eduard' });
+    expect(spy.mock.lastCall[0].length).toEqual(3);
+
+    arrayStore.updateItem({ id: 3, name: 'Margaret' });
+    expect(spy.mock.lastCall[0][2].name).toEqual('Margaret');
+
+    arrayStore.removeItem({ id: 3, name: 'Margaret' });
+    expect(spy.mock.lastCall[0].length).toEqual(2);
+
+    arrayStore.removeItemById(1);
+    expect(spy.mock.lastCall[0].length).toEqual(1);
+
+    arrayStore.destroy();
+    expect(spy).toHaveBeenCalledTimes(5);
+    arrayStore.addItem({ id: 3, name: 'Eduard' });
+    expect(spy).toHaveBeenCalledTimes(5);
   });
 });
--- packages/rxjs-state-store/src/lib/rxjs-state-store.ts
+++ packages/rxjs-state-store/src/lib/rxjs-state-store.ts
@@ -1,3 +1,114 @@
-export function rxjsStateStore(): string {
-  return 'rxjs-state-store';
+import { deepPatch } from 'js-deep-patch';
+import { BehaviorSubject, Observable, Subject, takeUntil } from 'rxjs';
+
+export class StateStore<T> {
+  private destroy$ = new Subject<void>();
+  private _value$: BehaviorSubject<T>;
+  public value$: Observable<T>;
+  public get value(): T {
+    return this._value$.value;
+  }
+
+  constructor(initialValue: T) {
+    this._value$ = new BehaviorSubject(initialValue);
+    this.value$ = this._value$.asObservable().pipe(takeUntil(this.destroy$));
+  }
+
+  public destroy() {
+    this.destroy$.next();
+  }
+
+  public update(value: T) {
+    this._value$.next(value);
+  }
+
+  /**
+   * Patch an object value
+   */
+  public patch(partialValue: Partial<T>): T {
+    if (!this.isObject(this.value)) {
+      throw new Error('[StateStore::patch] value is not an object');
+    }
+
+    const patchedValue = deepPatch(this.value, partialValue) as T;
+    this.update(patchedValue);
+
+    return patchedValue;
+  }
+
+  /**
+   * Remove an item from an array by id
+   */
+  public removeItemById(id: unknown) {
+    this.removeItem({ id });
+  }
+
+  /**
+   * Remove an item from an array
+   */
+  public removeItem(item: Record<any, unknown>): void {
+    if (!Array.isArray(this.value)) {
+      throw new Error('[StateStore::removeItem] value is not an array');
+    }
+
+    // TODO: this may remove multiple items, could be improved
+    const updatedArray = (this.value as any[]).filter((el) => {
+      if (el.id && item['id']) {
+        // Remove based on id, if possible
+        return el.id !== item['id'];
+      } else {
+        // Otherwise just compare
+        return el !== item;
+      }
+    }) as unknown as T;
+
+    this.update(updatedArray);
+  }
+
+  /**
+   * Update an item in an array
+   */
+  public updateItem(item: Record<any, unknown>) {
+    if (!Array.isArray(this.value)) {
+      throw new Error(`[StateStore::updateItem] value is not an array`);
+    }
+    if (!item['id']) {
+      throw new Error(`[StateStore::updateItem] item doesn't have an id`);
+    }
+
+    const updatedValues = this.value.map((v) => {
+      if (v.id === item['id']) {
+        return item;
+      } else {
+        return v;
+      }
+    });
+
+    this.update(updatedValues as unknown as T);
+  }
+
+  /**
+   * Add an item to an array
+   */
+  public addItem(item: Record<any, unknown>): void {
+    if (!Array.isArray(this.value)) {
+      throw new Error('[StateStore::addItem] value is not an array');
+    }
+
+    // Check, whether we already have the same item
+    if (item['id']) {
+      const sameItem = (this.value as any[]).find((el) => el.id === item['id']);
+
+      if (sameItem) {
+        return; // It's already added
+      }
+    }
+
+    // Add the new item
+    this.update([...this.value, item] as unknown as T);
+  }
+
+  private isObject(value: unknown): boolean {
+    return typeof value === 'object' && !Array.isArray(value) && value !== null;
+  }
 }
